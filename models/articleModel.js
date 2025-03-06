750b91093d1d42101795e29cac41b3d26d4b3477
MariammMagdy <magdymariam758@gmail.com>
2025-03-06 23:03:37 +0200

    update

--- models/articleModel.js
+++ models/articleModel.js
@@ -10,20 +10,20 @@ const ArticleSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-const setImageURL = (doc) => {
-  if (doc.image) {
-    const imageUrl = `${process.env.BASE_URI}/articles/${doc.image}`;
-    doc.image = imageUrl;
-  }
-};
-// findOne, findAll and update
-ArticleSchema.post("init", (doc) => {
-  setImageURL(doc);
-});
+// const setImageURL = (doc) => {
+//   if (doc.image) {
+//     const imageUrl = `${process.env.BASE_URI}/articles/${doc.image}`;
+//     doc.image = imageUrl;
+//   }
+// };
+// // findOne, findAll and update
+// ArticleSchema.post("init", (doc) => {
+//   setImageURL(doc);
+// });
 
-// create
-ArticleSchema.post("save", (doc) => {
-  setImageURL(doc);
-});
+// // create
+// ArticleSchema.post("save", (doc) => {
+//   setImageURL(doc);
+// });
 
 module.exports = mongoose.model("Article", ArticleSchema);
