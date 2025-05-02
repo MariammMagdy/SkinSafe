e6f1aa5a959d12cfcbfb4f4810ab780032c84aa8
MariammMagdy <magdymariam758@gmail.com>
2025-05-02 21:11:13 +0300

    update

--- middleware/validatorMiddleware.js
+++ middleware/validatorMiddleware.js
@@ -6,7 +6,7 @@ const validatorMiddleware =
     //Find the validation errors in this request and wraps then in an object with hand functions
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ essors: errors.array() });
+      return res.status(400).json({ errors: errors.array() });
     }
     //3lshan yn2l 3la el route b2a lw mfesh error
     next();
