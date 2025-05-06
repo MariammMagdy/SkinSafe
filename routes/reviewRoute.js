64fece8aedca210f98f067e51c96268ebd6e3134
ayadakhil2003 <aya.dakhil.2003@gmail.com>
2025-05-06 09:22:09 +0300

    Initial commit

--- routes/reviewRoute.js
+++ routes/reviewRoute.js
@@ -1,4 +1,61 @@
 const express = require("express");
+const {
+  createReviewValidator,
+  updateReviewValidator,
+  getReviewValidator,
+  deleteReviewValidator,
+} = require("../utils/validator/reviewValidator");
+
+const {
+  getReview,
+  getReviews,
+  getDoctorReviews,
+  createReview,
+  updateReview,
+  deleteReview,
+  createFilterObj,
+  setDoctorIdAndUserIdToBody,
+} = require("../services/reviewServices");
+
+const authService = require("../services/authServices");
+
+const router = express.Router({ mergeParams: true });
+
+// ⚡ ضعه قبل أي راوت فيه ":id"
+router.route("/doctorReviews/:id").get(getReviewValidator, getDoctorReviews);
+
+// بعدها الباقي
+router
+  .route("/")
+  .get(createFilterObj, getReviews)
+  .post(
+    authService.protect,
+    authService.allowedTo("user"),
+    setDoctorIdAndUserIdToBody,
+    createReviewValidator,
+    createReview
+  );
+
+router
+  .route("/:id")
+  .get(getReviewValidator, getReview)
+  .put(
+    authService.protect,
+    authService.allowedTo("user"),
+    updateReviewValidator,
+    updateReview
+  )
+  .delete(
+    authService.protect,
+    authService.allowedTo("user", "manager", "admin"),
+    deleteReviewValidator,
+    deleteReview
+  );
+
+module.exports = router;
+
+/*
+const express = require("express");
 
 const {
   createReviewValidator,
@@ -63,7 +120,7 @@ router
   );
 
 module.exports = router;
-
+*/
 /* const express = require("express");
 
 const {
