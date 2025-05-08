0454c30af8709834332ede24eac0e7b270c21f82
MariammMagdy <magdymariam758@gmail.com>
2025-05-08 03:27:11 +0300

    update

--- routes/recentSearchRoutes.js
+++ routes/recentSearchRoutes.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const { addDoctor, getDoctors } = require('../services/recentSearchServices');
 
+const authServer = require("../services/authServices");
+
 const router = express.Router();
+router.use(authServer.protect, authServer.allowedTo("user"));
 
 router.post('/addDoctor', addDoctor);
 router.get('/getDoctors', getDoctors);
