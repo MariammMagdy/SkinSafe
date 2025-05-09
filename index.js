d31407d6e4712d7cebee9b3b46d13ddcb4d325b8
MariammMagdy <magdymariam758@gmail.com>
2025-05-10 02:15:36 +0300

    update

--- index.js
+++ index.js
@@ -118,8 +118,7 @@ process.on("unhandledRejection", (err) => {
 });
 
 //console.log('KEY:', process.env.OPENWEATHER_API_KEY);
-console.log('KEY:', process.env.OPENWEATHER_API_KEY);
-console.log('KEY:', process.env.OPENWEATHER_API_KEY);
+
 
 
 
