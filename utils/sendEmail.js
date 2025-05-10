3bdd4af03241124f3e9322eddb910faad2a4c79f
ayadakhil2003 <aya.dakhil.2003@gmail.com>
2025-05-10 03:36:06 +0300

    Initial commit

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -39,7 +39,7 @@ const sendEmail = async (options) => {
     const transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
       port: process.env.EMAIL_PORT,
-      secure: process.env.EMAIL_PORT == 465, // true for port 465
+      secure: Number(process.env.EMAIL_PORT) === 465,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD,
