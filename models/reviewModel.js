be265bba7fadbbdf5307a7f7078d268cec3fa85b
ayadakhil2003 <aya.dakhil.2003@gmail.com>
2025-05-13 11:15:31 +0300

    Initial commit

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -34,25 +34,21 @@ reviewSchema.pre(/^find/, function (next) {
 // i do  any review this method call and calc avg and quantity in this doctor
 reviewSchema.statics.calcAverageRatingsAndQuantity = async function (doctorId) {
   const result = await this.aggregate([
-    // Stage 1 : get all reviews in specific product
+    // Step 1: Get all reviews for the specified doctor
     {
-      //       in review  model
       $match: { doctor: doctorId },
     },
-    // Stage 2: Grouping reviews based on productID and calc avgRatings, ratingsQuantity
+    // Step 2: Group by doctor ID and calculate average rating and count
     {
       $group: {
-        _id: "doctor",
-        avgRatings: { $avg: "$ratings" }, //on review model
-        ratingsQuantity: { $sum: 1 }, //1  is counter
+        _id: "$doctor",
+        avgRatings: { $avg: "$rating" }, // ✅ التصحيح هنا
+        ratingsQuantity: { $sum: 1 },
       },
     },
   ]);
 
-  // console.log(result);
-  //result=[{_id=product,avgRatings:num ,ratingsQuantity:num}]
   if (result.length > 0) {
-    //there are reviews on specific product
     await Doctor.findByIdAndUpdate(doctorId, {
       ratingsAverage: result[0].avgRatings,
       ratingsQuantity: result[0].ratingsQuantity,
