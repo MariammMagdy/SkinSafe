3a9b8cc3fc35073740daec357770e478f120c415
MariammMagdy <magdymariam758@gmail.com>
2025-07-07 01:15:29 +0300

    update

--- utils/createToken.js
+++ utils/createToken.js
@@ -1,8 +1,5 @@
 const jwt = require("jsonwebtoken");
 
-console.log("JWT_SECRET at token creation:", process.env.JWT_SECRET);
-
-
 const createToken = (payload) =>
   jwt.sign({ userId: payload }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRE_TIME,
